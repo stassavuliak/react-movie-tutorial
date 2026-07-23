c954c5dfc93643103e10c42e3d21240c768e0999
Stanislav Savuliak <stassavulak@gmail.com>
2026-07-23 19:47:15 +0200

    feat: add pages Home, Favorite

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import './App.css'
+import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
-      <div>
-        hello world
-      </div>
-      <Text display="whats up"></Text>
-      <Text display="hello"></Text>
+      <Home/>
     </>
  
   )
 }
 
-function Text({display}) {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
-  )
-}
 
 export default App
