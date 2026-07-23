24bcdcd413e4779a70f30babe3d992c39ed6d13f
Stanislav Savuliak <stassavulak@gmail.com>
2026-07-23 22:06:09 +0200

    feat: add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import Favorites from "./pages/Favorites"
 import Home from "./pages/Home"
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <Home/>
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
  
   )
 }
